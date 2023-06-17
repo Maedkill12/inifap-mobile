62e661099fd78987d883295a1427d5ca783d732e
maed <maed012014@gmail.com>
2023-06-17 17:38:02 -0600

    Setup navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import React from "react";
+import GradientBackground from "../components/GradientBackground";
+
+const HomeScreen = () => {
+  return (
+    <GradientBackground>
+      <Text>Home</Text>
+    </GradientBackground>
+  );
+};
+
+export default HomeScreen;
